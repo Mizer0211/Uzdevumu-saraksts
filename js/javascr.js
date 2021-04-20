0f39ea1e69dab6a1ff527efd19f3f48ff0d1db3a
Mizer0211 <Sanjok60660@gmail.com>
2021-04-20 15:16:04 +0300

    virsraksts centra

--- js/javascr.js
+++
@@ -1,9 +0,0 @@
-const POP_UP = document.getElementById('PopUp');
-let uzdevumi = [];
-
-window.addEventListener('load', () => {
-    uzdevumi = JSON.parse(localStorage.getItem("uzdevumi") || "[]");
-    console.log(uzdevumi)
-    render();
-});
-
